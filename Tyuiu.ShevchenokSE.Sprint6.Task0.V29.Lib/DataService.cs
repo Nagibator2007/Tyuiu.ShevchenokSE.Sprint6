33f208349ddfa4e1443919d0031ad108d4dfd8f3
Nagibator2007 <shevchenokse@outlook.com>
2025-12-11 14:33:41 +0500

    Add project files.

---
+++ Tyuiu.ShevchenokSE.Sprint6.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.ShevchenokSE.Sprint6.Task0.V29.Lib
+{
+    public class DataService : ISprint6Task0V29
+    {
+        public double Calculate(int x)
+        {
+            return Math.Round((Math.Pow(x, 3) - 1) / (4 * Math.Pow(x, 2)), 3);
+        }
+    }
+}
---
+++ Tyuiu.ShevchenokSE.Sprint6.Task0.V29.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.ShevchenokSE.Sprint6.Task0.V29.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint6.Task0.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CalcCheck()
+        {
+            var result = new DataService();
+            Assert.AreEqual(0.722, result.Calculate(3));
+        }
+    }
+}
