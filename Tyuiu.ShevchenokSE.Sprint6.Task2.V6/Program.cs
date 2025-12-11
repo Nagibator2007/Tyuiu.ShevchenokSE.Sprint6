99ed32e216c7ef8d691e8783a7a2401b74620bc1
Nagibator2007 <shevchenokse@outlook.com>
2025-12-11 17:13:20 +0500

    fix

--- Tyuiu.ShevchenokSE.Sprint6.Task2.V6/Program.cs
+++ Tyuiu.ShevchenokSE.Sprint6.Task2.V6/Program.cs
@@ -1,4 +1,4 @@
-namespace Tyuiu.ShevchenokSE.Sprint6.Task2.V6
+namespace Tyuiu.ShevchenokSE.Sprint6.Task1.V21
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace Tyuiu.ShevchenokSE.Sprint6.Task2.V6
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+            Application.Run(new Form1());
         }
     }
 }
