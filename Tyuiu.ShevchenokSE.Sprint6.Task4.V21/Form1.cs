798308d9d22871911522501945c7e9889a233ebe
Nagibator2007 <shevchenokse@outlook.com>
2025-12-11 18:22:56 +0500

    Task4

---
+++ Tyuiu.ShevchenokSE.Sprint6.Task4.V21/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.ShevchenokSE.Sprint6.Task4.V21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
